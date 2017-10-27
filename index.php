246980765094ff5490034c3a2d12ac36bcd77115
flyysr <flyysr@163.com>
2017-10-27 21:25:46 +0800

    add a file
    PHP中错误与异常的日志记录用法分析

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(E_ALL); 
+ini_set("display_errors", 1); 
+
+function my_error_handler($errno,$errstr,$errfile,$errline) {
+	var_dump( func_num_args());
+	echo "<br/><pre>";
+	var_dump(func_get_args());
+	echo "errno: ".$errno."<br/>";
+	echo "err msg: ".$errstr."<br/>";
+	echo "err file: ".$errfile."<br/>";
+	echo "err line: ".$errline."<br/>";
+}
+set_error_handler('my_error_handler');
+
+function my_exception_handler($exception){
+	var_dump($exception->getMessage());
+	echo "<br/><pre>";
+	var_dump(func_get_args());
+}
+set_exception_handler('my_exception_handler');
+
+try {
+    echo "hehe";
+    var_dump($name);
+    //$n = 1 / 0; //warning: Division by zero in /data/tellidea/index.php on line 7
+    $n = 1 % 0;
+ } catch (ErrorException  $e) {
+  echo "catched", PHP_EOL;
+  }
+
