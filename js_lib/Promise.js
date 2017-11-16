a3073d9ca41d1291bef151a145440ff18b7b93cd
saynoer <fly@tellidea.com>
2017-11-16 15:53:32 +0800

    update

--- js_lib/Promise.js
+++ js_lib/Promise.js
@@ -1,8 +1,71 @@
-
-
+/*
 function Promise(fn){
 
-	var value = null, deferreds = [], state = 'pending';
+	var value = null, deferreds = [];
+	var state = 'pending';
+	function resolve(newValue){
+		value = newValue;
+		state = 'fulfilled';
+		setTimeout(function(){
+			deferreds.forEach(function(deferred){
+			deferred(value);
+		});
+		}, 0);
+		
+	}
+
+	this.then = function(onFulfilled){
+		if(state == 'pending'){
+			deferreds.push(onFulfilled);
+			return this;
+		}
+		onFulfilled(value);
+		return this;
+	}
+	fn(resolve);
+}
+
+
+var myPromise = new Promise(function(resolve){
+
+	setTimeout(function(){
+		resolve('fly');
+	}, 1000);
+});
+
+myPromise.then(function(value){
+	console.log(value);
+	return new Promise(function(resolve){
+		resolve('pxk');
+	});
+
+}).then(function(value){
+	console.log(value);
+});*/
+
+/*----------------  new version , complish the Chain of Promise!----------------*/
+function Promise(fn){
+
+	var value = null, deferreds = [];
+	var state = 'pending';
+	function resolve(newValue){
+		if(newValue && (typeof newValue == 'object' || typeof newValue == 'function')){
+			var then = newValue.then;
+			if(typeof then == 'function'){
+				then.call(value, resolve);
+				return;
+			}
+		}
+
+		value = newValue;
+		state = 'fulfilled';
+		setTimeout(function(){
+			deferreds.forEach(function(deferred){
+			handle(deferred);
+		});
+		}, 0);
+		
+	}
 
 	this.then = function(onFulfilled){
 		return new Promise(function(resolve){
@@ -11,51 +74,36 @@ function Promise(fn){
 				resolve: resolve
 			});
 		});
-	};
-
-	function handle(deferred){
-		if(state == 'pending'){
-			deferreds.push(deferred);
-			return;
-		}
-		var ret = deferred.onFulfilled(value);
-		deferred.resolve(ret);
 	}
 
-	function resolve(newValue){
-		if(newValue && (typeof(newValue) =='object' || typeof(newValue) =='function')){
-			var then = newValue.then;
-			if(typeof then == 'function'){
-				then.call(newValue, resolve);
-				return;
-			}
+	function handle(onFulfilled){
+		if(state == 'pending'){
+			deferreds.push(onFulfilled);
+			return;
 		}
-		state = 'fulfilled';
-		value = newValue;
-		setTimeout(function(){
-			deferreds.forEach(function(deferred){
-				handle(deferred);
-			});
-		}, 0);
+		var ret = onFulfilled.onFulfilled(value);
+		onFulfilled.resolve(ret);
+		return;
 	}
 
 	fn(resolve);
 }
 
+
+
 var myPromise = new Promise(function(resolve){
+
 	setTimeout(function(){
-		resolve("fly");
-	}, 3000);
+		resolve('fly');
+	}, 1000);
 });
 
-myPromise.then(function(data){
-	alert(data);
-});
 myPromise.then(function(value){
-    console.log(value);
-    return new Promise(function(resolve){
-        resolve("second promise");
-    })
-}).then(function(data){
-	alert(data);
+	console.log(value);
+	return new Promise(function(resolve){
+		resolve('pxk');
+	});
+
+}).then(function(value){
+	console.log(value);
 });
